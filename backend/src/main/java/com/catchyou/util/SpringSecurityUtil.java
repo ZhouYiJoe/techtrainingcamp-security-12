d8962529a486911c2936aea96a33d6f8c450edcc
ZhouYiJoe <55095693+ZhouYiJoe@users.noreply.github.com>
2022-11-12 22:48:27 +0800

    增加获取当前登录用户的用户类型的方法

--- backend/src/main/java/com/catchyou/util/SpringSecurityUtil.java
+++ backend/src/main/java/com/catchyou/util/SpringSecurityUtil.java
@@ -1,10 +1,15 @@
 package com.catchyou.util;
 
+import com.catchyou.constant.UserType;
 import com.catchyou.pojo.dto.LoginUser;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SpringSecurityUtil {
     public static String getCurrentUserId() {
         return ((LoginUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+    }
+
+    public static UserType getCurrentUserType() {
+        return ((LoginUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUserType();
     }
 }
